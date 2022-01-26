00b2ae9a79422c9dea47b45638b2216d15b996f4
israel Kamunga <19kk145@esisalama.org>
2022-01-26 09:24:34 +0100

    Pratique des conditions

---
+++ action.php
@@ -0,0 +1,3 @@
+<?php
+$nom = $_POST['name'];
+echo "nom : ".$nom;
--- index.php
+++ index.php
@@ -7,17 +7,17 @@
   <title>Document</title>
 </head>
 <body>
-      <form action="Controller/Controller.php" method="POST">
-        Nom: <input type="type" placeholder="tapez votre nom" name="name"><br>
-        PostNom : <input type="text" placeholder="tapez votre PostNom" name="secondname"><br>
-        Prenom : <input type="text" placeholder="tapez votre Preom" name="lastname"><br>
-        Matricule : <input type="text" placeholder="tapez votre Matricule" name="Matricule"><br>
+      <form action="action.php" method="POST">
+        Nom: <input type="type" placeholder="  Tapez votre nom" name="name"><br>
+        PostNom : <input type="text" placeholder="  Tapez votre PostNom" name="secondname"><br>
+        Prenom : <input type="text" placeholder="  Tapez votre Prenom" name="lastname"><br>
+        Matricule : <input type="text" placeholder="  Tapez votre Matricule" name="Matricule"><br>
         Genre : <select name="genre" id="">
           <option value="Homme">Masculin</option>
           <option value="Femme">Feminin</option>
         </select><br>
-        Promotion : <input type="text" placeholder="tapez votre Promotion" name="Promotion"><br>
-        Email : <input type="email" placeholder="tapez votre adresse mail" name="Email"><br>
+        Promotion : <input type="text" placeholder="  Tapez votre Promotion" name="Promotion"><br>
+        Email : <input type="email" placeholder="  Tapez votre adresse mail" name="Email"><br>
         <button type="submit" value="submit">soumettre</button>
       </form>
 </body>
